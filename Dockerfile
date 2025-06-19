51d20acb87e05b9530aca721197d148d91be4649
RudolfDG <rudolf.degeijter@meemoo.be>
2025-06-19 06:29:11 +0200

    C1P19-39 add mh 2.0 complex templates  (#2)
    * C1P19 Add file structure for jinja templates.
    
    * Add MH film sip creation as basic.
    
    * Add dummy sip for testing purposes.
    
    * Remove prints
    
    * Add pid generation

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,15 @@
-FROM python:3.12
+FROM python:3.12-slim
+
+
 
 # Make a new group and user so we don't run as root.
-RUN addgroup --system appgroup && adduser --system appuser --ingroup appgroup
+# RUN addgroup --system appgroup && adduser --system appuser --ingroup appgroup
 
 WORKDIR /app
 
 # Let the appuser own the files so he can rwx during runtime.
-COPY --chown=appuser:appgroup . .
+# COPY --chown=appuser:appgroup . .
+COPY . .
 
 # We install all our Python dependencies. Add the extra index url because some
 # packages are in the meemoo repo.
@@ -14,7 +17,7 @@ RUN pip install -e . \
     --extra-index-url http://do-prd-mvn-01.do.viaa.be:8081/repository/pypi-all/simple \
     --trusted-host do-prd-mvn-01.do.viaa.be
 
-USER appuser
+# USER appuser
 
 # This command will be run when starting the container. It is the same one that can be used to run the application locally.
 CMD [ "python", "main.py"]
--- app/app.py
+++ app/app.py
@@ -3,6 +3,21 @@ from viaa.configuration import ConfigParser
 from viaa.observability import logging
 
 from app.services.pulsar import PulsarClient
+from app.services.pid import PidClient
+
+import os
+import shutil
+import uuid
+import zipfile
+from datetime import datetime
+from pathlib import Path
+import json
+from jinja2 import Environment, FileSystemLoader
+
+from app.helpers.template import generate_mets_from_sip
+
+from app.helpers.dummy_sip import f
+
 
 APP_NAME = "sipin-mh-sip-creator-v2"
 
@@ -17,8 +32,13 @@ class EventListener:
         Initializes the EventListener with configuration, logging, and Pulsar client.
         """
         config_parser = ConfigParser()
+        self.config = config_parser.app_cfg
+
         self.log = logging.get_logger(__name__, config=config_parser)
         self.pulsar_client = PulsarClient()
+        self.pid_client = PidClient()
+
+        self.app_config = self.config["mh-sip-creator"]
 
     def handle_incoming_message(self, event: Event):
         """
@@ -31,7 +51,54 @@ class EventListener:
             self.log.info(f"Dropping non successful event: {event.get_data()}")
             return
 
-        self.log.info(f"Start handling of {event.get_attributes()['subject']}.")
+        event_attributes = event.get_attributes()
+        
+        # Subject contains the path to the unzipped bag
+        subject = event_attributes.get("subject")
+        if subject is None:
+            self.log.error("Invalid event: subject is missing.")
+            return
+        self.log.info(f"Start handling of {subject}.")
+        
+        event_data = event.get_data()
+        
+        sip_metadata = event_data["metadata"]
+        
+        # todo: deserialize sip_metadata to a real SIP.py object
+        # For now, we will use a dummy SIP object for testing purposes
+        # json_ld_graph = json.loads(graph_string)["@graph"]
+
+        # for model, model_dict in zip(models, json_ld_graph):
+        #     deserialized = model.__class__.model_validate(model_dict)
+        
+
+        
+        sip = f
+        
+        pid = self.pid_client.get_pid()
+
+        files_path = Path(self.app_config["aip_folder"], pid)
+        files_path.mkdir(parents=True, exist_ok=True)
+
+        for i in range(len(sip.representations)):
+            shutil.copytree(
+                Path(subject, f"data/representations/representation_{i+1}/data"),
+                Path(files_path, f"representation_{i+1}"),
+                copy_function=shutil.move,
+            )
+
+        try:
+
+            mets_xml = generate_mets_from_sip(sip, pid)
+
+            # Save the generated XML to a file
+            mets_file_path = files_path / "mets.xml"
+            with open(mets_file_path, "w") as mets_file:
+                mets_file.write(mets_xml)
+
+            self.log.info(f"Generated METS XML file at {mets_file_path}")
+        except Exception as e:
+            self.log.error(f"Failed to generate METS XML: {e}")
 
     def start_listening(self):
         """
